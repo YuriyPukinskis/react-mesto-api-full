c8421197676c0648644d93be9853be5580aa1342
Yuriy Pukinskis <pukinskis.yuriy@andex.ru>
2021-05-24 04:23:13 +0300

    commitNew

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://pr15front.students.nomoredomains.club/',
   credentials: true,
 }));
 app.use(requestLogger);
