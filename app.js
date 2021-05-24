56ce9a94bb099079e4025eaa5503090a6ec19750
Yuriy Pukinskis <pukinskis.yuriy@andex.ru>
2021-05-24 22:34:52 +0300

    com

--- app.js
+++ app.js
@@ -27,11 +27,19 @@ app.listen(PORT, () => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors({
-  origin: true,
-  exposedHeaders: '*',
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: true,
+//   exposedHeaders: '*',
+//   credentials: true,
+// }));
+
+app.options('*', (req, res) => {
+  res.set('Access-Control-Allow-Origin', 'https://pr15front.students.nomoredomains.club');
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
+  res.set('Access-Control-Allow-Methods', ['PUT', 'GET', 'POST', 'DELETE', 'PATCH']);
+  res.set('Access-Control-Allow-Credentials', 'true');
+  res.send('ok');
+});
 app.use(requestLogger);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
